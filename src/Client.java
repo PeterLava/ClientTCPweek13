2e937227eb2f426f6be26bcb370f8b647ad6d642
Peter <pede.13@live.dk>
2020-03-29 18:53:38 +0200

    refactoring

--- src/Client.java
+++ src/Client.java
@@ -3,24 +3,20 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-
-
     public static void main(String[] args) throws IOException {
-        Socket c1 = new Socket("localhost", 4999);
-        PrintWriter printWriter = new PrintWriter(c1.getOutputStream());
-        InputStreamReader inputStreamReader = new InputStreamReader(c1.getInputStream());
-        InputStreamReader inputstring = new InputStreamReader(System.in);
-        BufferedReader bufferedReaderscanner = new BufferedReader(inputstring);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
+        Socket socket = new Socket("localhost", 4999);
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        InputStreamReader serverInput = new InputStreamReader(socket.getInputStream());
+        BufferedReader serverReader = new BufferedReader(serverInput);
+        InputStreamReader userInput = new InputStreamReader(System.in);
+        BufferedReader userReader = new BufferedReader(userInput);
         while(true) {
-
-                if(bufferedReader.ready()){
-                    String input1 = bufferedReader.readLine();
+                if(serverReader.ready()){
+                    String input1 = serverReader.readLine();
                     System.out.println(input1);
                 }
-                if(bufferedReaderscanner.ready()){
-                   String message = bufferedReaderscanner.readLine();
+                if(userReader.ready()){
+                    String message = userReader.readLine();
                     printWriter.println(message);
                     printWriter.flush();
                 }
